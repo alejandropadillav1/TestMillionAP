d649878533206c19d30f012cbe6469906fbe7b5d
Alejandro Padilla V <alejandro.padilla@tecna-ice.com>
2021-04-10 10:38:21 -0500

    Update the code documentation

--- TestMillionAP/Services/RealEstateXPOServices.cs
+++ TestMillionAP/Services/RealEstateXPOServices.cs
@@ -12,7 +12,7 @@ namespace TestMillionAP.Services
 {
     /// <summary>
     ///   Model Class for XPO (ORM), see the benchmarking between EF - EF Core - XPO - https://community.devexpress.com/blogs/xpo/archive/2018/07/17/xpo-a-simple-benchmark-against-ef-6-and-ef-core-update.aspx  The XPO ORM doesn't required to declare NotifyPropertyChanged event because of XPO ORM already pre built using with SetPropertyValue
-    ///   automatically.
+    ///   automatically. The ORM XPO can connect with several database engine, i.e. MySQL, SQL Server, Oracle, MS Access, Google Big Query, Teradata, Amazon Redshift, Sybase, PostgreSQL, Firebird (the XPO only is a open source and free of charge).
     /// </summary>
     public class RealEstateXPOServices : IRealEstateServices
     {
